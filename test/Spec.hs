6a784c4bdca93b2293f44981916b8cce620966b0
Andrási László <laszlo.andrasi@balabit.com>
2017-10-24 17:12:47 +0200

    snake turning and steps mvp
    Signed-off-by: Andrási László <laszlo.andrasi@balabit.com>

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,10 +9,17 @@ equals left right message = let result = if left == right
 main :: IO ()
 main = do
   let theEmptyWorld = Hs.emptyWorld (Dimension 10 10)
-  let newSnake = Hs.Snake (Id 0) North (Coordinate 0 0) []
+  let newSnake = Hs.Snake (Id 0) North (Coordinate 5 5) [(Coordinate 5 6), (Coordinate 4 6)]
+  let turnedSnake = Hs.Snake (Id 0) East (Coordinate 5 5) [(Coordinate 5 6), (Coordinate 4 6)]
   let anotherSnake = Hs.Snake (Id 1) North (Coordinate 0 0) []
+  let steppedSnake = Hs.Snake (Id 0) North (Coordinate 5 4) [(Coordinate 5 5), (Coordinate 5 6)]
   let worldWithNewSnake = theEmptyWorld {snakes = [newSnake]}
   let worldWithSnakes = theEmptyWorld {snakes = [newSnake, anotherSnake]}
+  let worldWithTurnedSnake = theEmptyWorld {snakes = [turnedSnake, anotherSnake]}
+  let worldWithSteppedSnake = theEmptyWorld {snakes = [steppedSnake]}
+
   equals (updateWorld theEmptyWorld (AddSnake newSnake)) worldWithNewSnake "NewSnake should be added to the world."
   equals (updateWorld worldWithNewSnake (AddSnake newSnake)) worldWithNewSnake "NewSnake should be added only with unused id."
   equals (updateWorld worldWithSnakes (RemoveSnake (snakeId anotherSnake))) worldWithNewSnake "NewSnake should be removed from the world."
+  equals (updateWorld worldWithSnakes (TurnSnake (Id 0) East)) worldWithTurnedSnake "A snake should be turned east"
+  equals (updateWorld worldWithNewSnake Step) worldWithSteppedSnake "All snakes should move"
