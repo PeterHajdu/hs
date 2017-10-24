1f9bb089102a61da8a76ea333b2c01e698bf8548
Peter Hajdu <peter.hajdu@balabit.com>
2017-10-24 17:29:19 +0200

    split up main test function
    Signed-off-by: Peter Hajdu <peter.hajdu@balabit.com>

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,20 +6,41 @@ equals left right message = let result = if left == right
                                          else "FAIL"
                             in putStrLn (message ++ " -> " ++ result)
 
-main :: IO ()
-main = do
-  let theEmptyWorld = Hs.emptyWorld (Dimension 10 10)
-  let newSnake = Hs.Snake (Id 0) North (Coordinate 5 5) [(Coordinate 5 6), (Coordinate 4 6)]
-  let turnedSnake = Hs.Snake (Id 0) East (Coordinate 5 5) [(Coordinate 5 6), (Coordinate 4 6)]
-  let anotherSnake = Hs.Snake (Id 1) North (Coordinate 0 0) []
-  let steppedSnake = Hs.Snake (Id 0) North (Coordinate 5 4) [(Coordinate 5 5), (Coordinate 5 6)]
-  let worldWithNewSnake = theEmptyWorld {snakes = [newSnake]}
-  let worldWithSnakes = theEmptyWorld {snakes = [newSnake, anotherSnake]}
-  let worldWithTurnedSnake = theEmptyWorld {snakes = [turnedSnake, anotherSnake]}
-  let worldWithSteppedSnake = theEmptyWorld {snakes = [steppedSnake]}
+theEmptyWorld :: World
+theEmptyWorld = Hs.emptyWorld (Dimension 10 10)
 
+newSnake :: Hs.Snake
+newSnake = Hs.Snake (Id 0) North (Coordinate 5 5) [(Coordinate 5 6), (Coordinate 4 6)]
+
+worldWithNewSnake :: World
+worldWithNewSnake = theEmptyWorld {snakes = [newSnake]}
+
+worldWithSnakes :: World
+worldWithSnakes = theEmptyWorld {snakes = [newSnake, anotherSnake]}
+
+anotherSnake :: Snake
+anotherSnake = Hs.Snake (Id 1) North (Coordinate 0 0) []
+
+snakeMovement :: IO ()
+snakeMovement = do
+  let steppedSnake = Hs.Snake (Id 0) North (Coordinate 5 4) [(Coordinate 5 5), (Coordinate 5 6)]
+  let worldWithSteppedSnake = theEmptyWorld {snakes = [steppedSnake]}
+  equals (updateWorld worldWithNewSnake Step) worldWithSteppedSnake "All snakes should move"
+
+snakeTurning :: IO ()
+snakeTurning = do
+  let turnedSnake = Hs.Snake (Id 0) East (Coordinate 5 5) [(Coordinate 5 6), (Coordinate 4 6)]
+  let worldWithTurnedSnake = theEmptyWorld {snakes = [turnedSnake, anotherSnake]}
+  equals (updateWorld worldWithSnakes (TurnSnake (Id 0) East)) worldWithTurnedSnake "A snake should be turned east"
+
+addingRemovingSnakes :: IO ()
+addingRemovingSnakes = do
   equals (updateWorld theEmptyWorld (AddSnake newSnake)) worldWithNewSnake "NewSnake should be added to the world."
   equals (updateWorld worldWithNewSnake (AddSnake newSnake)) worldWithNewSnake "NewSnake should be added only with unused id."
   equals (updateWorld worldWithSnakes (RemoveSnake (snakeId anotherSnake))) worldWithNewSnake "NewSnake should be removed from the world."
-  equals (updateWorld worldWithSnakes (TurnSnake (Id 0) East)) worldWithTurnedSnake "A snake should be turned east"
-  equals (updateWorld worldWithNewSnake Step) worldWithSteppedSnake "All snakes should move"
+
+main :: IO ()
+main = do
+  snakeMovement
+  snakeTurning
+  addingRemovingSnakes
